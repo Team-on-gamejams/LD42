5f76e7e5f5d8bc9fa970482c69e092afc4d46b28
Team-on <timonsol13@gmail.com>
2018-08-12 00:29:53 +0300

    Scrole background

--- README.md
+++ README.md
@@ -1,27 +1,36 @@
 # LD42
 ### [Ludum Dare Compo #42. Theme: "Running out of space"](https://ldjam.com/events/ludum-dare/42)
-*source* - game that I upload as finished ld42 compo
+*source* - game that I submit
 
 *sourceFirstIdea* - fruitless attempt. I cant made normal sidescrolling and bullet shoots.
 
 #### THEME
+S.P.A.C.E., inc. steal all SPACES from keyboards. They build SPACEship with more free SPACE inside it. But, unfortunely, they took you with your SPACE too. Arm with SPACE and slash all S.P.A.C.E. minions!
 
 
 #### GOAL
+Run as long as you can
 
 
 #### HOW TO PLAY 
+*Space* - attack
+*W* - jump
+*S* - roll
 
 
 ### OTHER
+-----
 #### LINK TO GAME ENTRY
+https://ldjam.com/events/ludum-dare/42/$100574
 
 
 #### TOOLS USED
  * Visual Studio 2017
  * Photoshop CS2
  * Paint
+ * sfxr
 
+ 
 #### ABOUT CODE
  * C#, WPF
  * [WpfAnimatedGif](https://github.com/XamlAnimatedGif/WpfAnimatedGif)
--- source/MainWindow.xaml.cs
+++ source/MainWindow.xaml.cs
@@ -18,8 +18,47 @@ namespace ld42 {
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
 	public partial class MainWindow : Window {
+		ulong tick = 1;
+
+		Image backgroundImage1;
+		Image backgroundImage2;
+		short backgroundImagePos = 0;
+
 		public MainWindow() {
 			InitializeComponent();
+
+			backgroundImage1 = new Image() { Source=new BitmapImage(new Uri(@"Resources\img\road.png", UriKind.Relative))};
+			gameCanvas.Children.Add(backgroundImage1);
+			Canvas.SetLeft(backgroundImage1, 0);
+			backgroundImage2 = new Image() { Source = new BitmapImage(new Uri(@"Resources\img\road.png", UriKind.Relative)) };
+			gameCanvas.Children.Add(backgroundImage2);
+			Canvas.SetLeft(backgroundImage2, 1000);
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e) {
+			System.Timers.Timer t = new System.Timers.Timer() {
+				AutoReset = true,
+				Interval = 10,
+				Enabled = false,
+			};
+
+			t.Elapsed += (a, b) => {
+				System.Windows.Application.Current.Dispatcher.Invoke((Action)delegate {
+					try {
+						Canvas.SetLeft(backgroundImage1, Canvas.GetLeft(backgroundImage1) - 1);
+						if (Canvas.GetLeft(backgroundImage1) <= -1000)
+							Canvas.SetLeft(backgroundImage1, 1000);
+						Canvas.SetLeft(backgroundImage2, Canvas.GetLeft(backgroundImage2) - 1);
+						if (Canvas.GetLeft(backgroundImage2) <= -1000)
+							Canvas.SetLeft(backgroundImage2, 1000);
+					}
+					catch(Exception ex) {
+						MessageBox.Show(ex.Message + '\n' + ex.StackTrace);
+					}
+				});
+			};
+
+			t.Start();
 		}
 	}
 }
